b87808115676fc430ce134d5de7a14722fc36f6d
Vinicius Benedito <beneditoloura@gmail.com>
2025-07-15 06:26:19 -0300

    feat: validate if exists roomId

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,4 +1,16 @@
+import { useParams, Navigate } from "react-router-dom";
+
+type RoomParams = {
+  roomId: string;
+};
+
 export function Room() {
+  const params = useParams<RoomParams>();
+
+  if (!params.roomId) {
+    return <Navigate to="/" replace />;
+  }
+
   return (
     <div>
       <h1>Room Page</h1>
