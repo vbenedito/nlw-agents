d5d2a064bb8d778adb8f95823111fc834d6aedb3
Vinicius Benedito <beneditoloura@gmail.com>
2025-07-16 07:31:39 -0300

    feat: create room page

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,39 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
-
-type GetRoomsAPIResponse = Array<{
-  id: string;
-  name: string;
-}>;
+import { RoomCard } from "@/components/room-card";
+import { useRooms } from "@/http/use-rooms";
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["get-rooms"],
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3333/rooms");
-      const result: GetRoomsAPIResponse = await response.json();
-
-      return result;
-    },
-  });
+  const { data } = useRooms();
 
   return (
-    <div>
-      <h1>Create a new room</h1>
-
-      {isLoading && <p>Loading rooms...</p>}
-      <div className="flex flex-col gap-2">
-        {data?.map((room) => {
-          return (
-            <Link key={room.id} to={`/room/${room.id}`}>
-              {room.name}
-            </Link>
-          );
-        })}
+    <div className="min-h-screen py-8 px-4">
+      <div className="m-auto max-w-4xl">
+        <div className="grid gap-8 grid-cols-2 items-start">
+          <RoomCard data={data} />
+        </div>
       </div>
-      <Link className="underline" to="/room">
-        Acessar sala
-      </Link>
     </div>
   );
 }
