a923d98576f1a7a1cb6aaf334af693397e2f5481
Vinicius Benedito <beneditoloura@gmail.com>
2025-07-16 07:22:13 -0300

    refactor: change type imports

--- web/src/components/room-card/index.tsx
+++ web/src/components/room-card/index.tsx
@@ -1,7 +1,7 @@
 import { Card } from "../ui/card";
 import { RoomCardHeader } from "./room-card-header";
 import { RoomCardContent } from "./room-card-content";
-import type { GetRoomsAPIResponse } from "@/types";
+import type { GetRoomsAPIResponse } from "@/http/types";
 
 export function RoomCard({ data }: { data: GetRoomsAPIResponse }) {
   return (
