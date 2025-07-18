66266a7b0072b22716fa3f6487676bdab92ef47a
Vinicius Benedito <beneditoloura@gmail.com>
2025-07-18 06:32:06 -0300

    refactor: change rooms to room link redirect

--- web/src/components/room-card/room-card-content.tsx
+++ web/src/components/room-card/room-card-content.tsx
@@ -13,7 +13,7 @@ export function RoomCardContent({ data }: { data: GetRoomsAPIResponse }) {
           <Link
             key={room.id}
             className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent/50"
-            to={`/rooms/${room.id}`}
+            to={`/room/${room.id}`}
           >
             <div className="flex-1 flex flex-col gap-1">
               <h3 className="font-medium">{room.name}</h3>
