46ea1d5286a868dbf367650de3ec1f17c876a245
Vinicius Benedito <beneditoloura@gmail.com>
2025-07-18 06:16:20 -0300

    feat: update request

--- web/src/http/use-create-room.ts
+++ web/src/http/use-create-room.ts
@@ -1,8 +1,9 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { CreateRoomRequest } from "./types/create-room-request";
 import type { CreateRoomResponse } from "./types/create-room-response";
 
 export function useCreateRoom() {
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: async (data: CreateRoomRequest) => {
       const response = await fetch("http://localhost:3333/rooms", {
@@ -17,5 +18,8 @@ export function useCreateRoom() {
 
       return result;
     },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['get-rooms']})
+    }
   })
 }
